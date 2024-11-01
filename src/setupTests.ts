8c998bd9f0b3fd94bfa64a2e2670abecfab1edbf
Aman Reddy p <reddyaman77.ar@gmail.com>
2024-11-01 18:38:24 +0530

    fixing deployment error

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
 // src/setupTests.ts
 import '@testing-library/jest-dom'; // for additional matchers
-import { expect } from 'vitest'; // Use Vitest's expect
+
 
 // If you need to add any custom matchers or setups, you can do it here.
